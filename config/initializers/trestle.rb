21989546b064877263a33000e9935697409bfd1f
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2024-02-24 22:02:06 +0100

    maj police et calendar

--- config/initializers/trestle.rb
+++ config/initializers/trestle.rb
@@ -3,7 +3,8 @@ Trestle.configure do |config|
   #
   # Set the page title shown in the main header within the admin.
   #
-  config.site_title = "Daphne Baudouin"
+  config.site_title = "DAPHNÉ BIIGA NWANAK <br>
+BAUDOUIN WOEHL"
 
   # Specify a custom image to be used in place of the site title for mobile and
   # expanded/desktop navigation. These images should be placed within your
