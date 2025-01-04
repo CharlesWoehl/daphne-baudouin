3b841e48e0907f38eb9e4036666fe3ab37f1c59b
Charles Woehl <122440168+CharlesWoehl@users.noreply.github.com>
2025-01-04 17:22:19 +0100

    maj

--- app/models/work.rb
+++ app/models/work.rb
@@ -1,5 +1,5 @@
 class Work < ApplicationRecord
   has_many_attached :photos
   belongs_to :category
-  has_many :calendars
+  has_many :calendars, dependent: :destroy # Supprime les calendars liés automatiquement
 end
